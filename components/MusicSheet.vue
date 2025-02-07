4ce0fab5fdd81fcb0b85dc74b57d6695175d6e4c
Kellen Baker <mkellenbaker@gmail.com>
2025-02-07 10:58:06 -0600

    set note colors

--- components/MusicSheet.vue
+++ components/MusicSheet.vue
@@ -1,21 +1,28 @@
 <script setup>
 import { Renderer, Stave, StaveNote, Voice, Formatter } from "vexflow";
+import { useNoteColors } from "~/composables/useNoteColors";
 
 const vexContainer = ref(null);
+const { noteColors } = useNoteColors();
 
 onMounted(() => {
   const renderer = new Renderer(vexContainer.value, Renderer.Backends.SVG);
   renderer.resize(400, 200);
   const context = renderer.getContext();
-  const stave = new Stave(10, 40, 300);
+  const stave = new Stave(10, 40, 350);
   stave.addClef("treble").setContext(context).draw();
-
+  stave.addTimeSignature("4/4").setContext(context).draw();
   const notes = [
-    new StaveNote({ keys: ["c/4"], duration: "q", color: "red" }),
-    new StaveNote({ keys: ["d/4"], duration: "q", color: "blue" }),
-    new StaveNote({ keys: ["e/4"], duration: "q", color: "green" }),
-    new StaveNote({ keys: ["f/4"], duration: "q", color: "orange" }),
+    new StaveNote({ keys: ["c/4"], duration: "q" }),
+    new StaveNote({ keys: ["d/4"], duration: "q" }),
+    new StaveNote({ keys: ["e/4"], duration: "q" }),
+    new StaveNote({ keys: ["f/4"], duration: "q" }),
   ];
+
+  notes[0].setStyle({ fillStyle: noteColors.c, strokeStyle: noteColors.c });
+  notes[1].setStyle({ fillStyle: noteColors.d, strokeStyle: noteColors.d });
+  notes[2].setStyle({ fillStyle: noteColors.e, strokeStyle: noteColors.e });
+  notes[3].setStyle({ fillStyle: noteColors.f, strokeStyle: noteColors.f });
 
   const voice = new Voice({ num_beats: 4, beat_value: 4 });
   voice.addTickables(notes);
---
+++ composables/useNoteColors.js
@@ -0,0 +1,15 @@
+export const useNoteColors = () => {
+  const noteColors = {
+    c: "#FE0201",
+    d: "#FF9907",
+    e: "#FFFC08",
+    f: "#9ACD68",
+    g: "#019897",
+    a: "#662AFF",
+    b: "#CB1FCC",
+  };
+
+  return {
+    noteColors,
+  };
+};
